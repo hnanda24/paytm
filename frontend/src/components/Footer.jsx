6ef799c3db8dd86feed1db6cb8efbcd3986a457d
hnanda24 <hnanda005@gmail.com>
2025-04-11 01:39:55 +0530

    footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#1e1b4b] text-white py-4">
+    <footer className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-4">
       <div className="container mx-auto text-center flex flex-col items-center">
-        <p className="text-sm text-gray-400">
+        <p className="text-sm">
           &copy; {new Date().getFullYear()} PayWallet. All rights reserved.
         </p>
-        <p className="text-sm text-gray-300">
+        <p className="text-sm">
           Built with ❤️ by{" "}
           <span className="font-bold hover:text-gray-100 transition">
             Hardik
